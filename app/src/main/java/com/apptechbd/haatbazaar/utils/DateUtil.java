ad58cc70f3f102ef803fedb03509baf2a5e68e2c
ahmedthossain <ahmedthossain@outlook.com>
2024-05-15 03:12:05 +0600

    feat: show the date range when filtering with daily/weekly/monthly in Analytics Screen

--- app/src/main/java/com/apptechbd/haatbazaar/utils/DateUtil.java
+++ app/src/main/java/com/apptechbd/haatbazaar/utils/DateUtil.java
@@ -1,13 +1,35 @@
 package com.apptechbd.haatbazaar.utils;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
 public class DateUtil {
     public static String getTodayDate() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd'th' MMMM, yyyy", Locale.getDefault());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM, yyyy", Locale.getDefault());
         Date today = new Date();
         return dateFormat.format(today);
+    }
+
+    public static String getLastPeriod(String period) {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM, yyyy", Locale.getDefault());
+
+        if (period.equals("week")) {
+            calendar.add(Calendar.DAY_OF_YEAR, -6); // Go back 6 days for the starting date
+            Date startDate = calendar.getTime();
+            Date endDate = new Date(); // End date is today
+
+            return dateFormat.format(startDate) + " - " + dateFormat.format(endDate);
+        } else if (period.equals("month")) {
+            calendar.add(Calendar.DAY_OF_YEAR, -29); // Go back 29 days for the starting date
+            Date startDate = calendar.getTime();
+            Date endDate = new Date(); // End date is today
+
+            return dateFormat.format(startDate) + " - " + dateFormat.format(endDate);
+        } else {
+            return "Invalid period";
+        }
     }
 }
--- app/src/main/java/com/apptechbd/haatbazaar/views/fragments/AnalyticsFragment.java
+++ app/src/main/java/com/apptechbd/haatbazaar/views/fragments/AnalyticsFragment.java
@@ -2,6 +2,7 @@ package com.apptechbd.haatbazaar.views.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -12,8 +13,14 @@ import com.apptechbd.haatbazaar.R;
 import com.apptechbd.haatbazaar.databinding.FragmentAnalyticsBinding;
 import com.apptechbd.haatbazaar.utils.DateUtil;
 
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+
+import java.util.List;
+
 public class AnalyticsFragment extends Fragment {
     private FragmentAnalyticsBinding binding;
+
     public AnalyticsFragment() {
         // Required empty public constructor
     }
@@ -21,12 +28,27 @@ public class AnalyticsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentAnalyticsBinding.inflate(inflater,container,false);
+        binding = FragmentAnalyticsBinding.inflate(inflater, container, false);
 
         binding.chipgroup.check(R.id.chip_daily);
         binding.textDateRange.setText(DateUtil.getTodayDate());
 
-        // Inflate the layout for this fragment
+        ChipGroup chipGroup = binding.chipgroup;
+        chipGroup.setOnCheckedStateChangeListener(new ChipGroup.OnCheckedStateChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull ChipGroup chipGroup, @NonNull List<Integer> list) {
+                String dateRange = "";
+                if (chipGroup.getCheckedChipId() == R.id.chip_weekly)
+                    dateRange = DateUtil.getLastPeriod("week");
+                else if (chipGroup.getCheckedChipId() == R.id.chip_monthly)
+                    dateRange = DateUtil.getLastPeriod("month");
+                else
+                    dateRange = DateUtil.getTodayDate();
+                binding.textDateRange.setText(dateRange);
+        }
+    });
+
+    // Inflate the layout for this fragment
         return binding.getRoot();
-    }
+}
 }
