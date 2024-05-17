a9b4c5014c81f104de338b4a2a0b4b8b4a1bcdbe
ahmedthossain <ahmedthossain@outlook.com>
2024-05-18 01:51:22 +0600

    fix: resolve issue with view binding in Add Activity screen

--- app/src/main/java/com/apptechbd/haatbazaar/views/activities/AddAccountActivity.java
+++ app/src/main/java/com/apptechbd/haatbazaar/views/activities/AddAccountActivity.java
@@ -9,14 +9,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.apptechbd.haatbazaar.R;
+import com.apptechbd.haatbazaar.databinding.ActivityAddAccountBinding;
+import com.apptechbd.haatbazaar.utils.PhoneNumberFormatter;
 
 public class AddAccountActivity extends AppCompatActivity {
-
+    private ActivityAddAccountBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_add_account);
+
+        binding = ActivityAddAccountBinding.inflate(getLayoutInflater(), getWindow().getDecorView().findViewById(android.R.id.content), false);
+
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
